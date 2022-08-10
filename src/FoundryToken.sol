4b9913186fa1e83e966269729ed25ea49f4a5b53
Carlos J. Ramirez <tomkat_cr@yahoo.com>
2022-08-09 22:42:47 -0500

    Add: test reto 1

--- src/FoundryToken.sol
+++ src/FoundryToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity >=0.8.0 <0.8.16;
 
 import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
--- test/FoundryToken.t.sol
+++ test/FoundryToken.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity >=0.8.0 <0.8.16;
 
 import "forge-std/Test.sol";
 
@@ -11,17 +11,22 @@ contract FoundryTokenTest is Test {
     FoundryToken internal token;
 
     address payable internal alice;
+    address payable internal bob;
 
     function setUp() public {
         utils = new Utilities();
         token = new FoundryToken(1000);
 
         // Create a user
-        address payable[] memory users = utils.createUsers(1);
+        address payable[] memory users = utils.createUsers(2);
 
         // Assign the user 0 as Alice
         alice = users[0];
         vm.label(alice, "Alice");
+
+        // Assign the user 1 as Bob
+        bob = users[1];
+        vm.label(bob, "Bob");
     }
 
     function testName() public {
@@ -42,4 +47,27 @@ contract FoundryTokenTest is Test {
         vm.expectRevert(bytes("Ownable: caller is not the owner"));
         token.mintFor(alice, 1000);
     }
+
+    function testSymbol() public {
+        assertEq(token.symbol(), "FTK");
+    }
+
+    function testTransferBalances() public {
+        // Funds to alice anbd bob
+        token.transfer(alice, 30);
+        token.transfer(bob, 10);
+
+        // Verify initial balances
+        assertEq(token.balanceOf(alice), 30);
+        assertEq(token.balanceOf(bob), 10);
+
+        // Alice transfer to bob
+        vm.startPrank(alice);
+        token.transfer(bob, 10);
+
+        // Verify final balances
+        assertEq(token.balanceOf(alice), 20);
+        assertEq(token.balanceOf(bob), 20);
+    }
+
 }
